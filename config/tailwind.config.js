c05d5c1b7cc08b59c3dc94f36e47d60436663297
Josiah <josiah.pilon@gmail.com>
2024-05-18 22:11:30 -0700

    style: for typical typography and elements. Styles for homepage.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,57 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  mode: "jit",
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/javascripts/**/*.js",
+    "./app/javascript/**/*.js",
   ],
   theme: {
+    fontSize: {
+      sm: ".5rem",
+      md: ".75rem",
+      lg: "1rem",
+      xl: "1.125rem",
+      "2xl": "1.5rem",
+      "3xl": "2rem",
+      "4xl": "2.75rem",
+      "5xl": "3rem",
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "background-black": "#030712",
+        "background-navy": "#111827",
+        "gray-50": "#f9fafb",
+        "gray-100": "#f3f4f6",
+        "gray-200": "#e5e7eb",
+        "gray-300": "#d1d5db",
+        "gray-400": "#9ca3af",
+        "gray-500": "#6b7280",
+        "gray-600": "#4b5563",
+        "gray-700": "#374151",
+        "gray-800": "#1f2937",
+        "gray-900": "#111827",
+        "gray-950": "#030712",
+        primary: "#FFD701",
+        // 'light': '#FFFAF4',
+        // 'gray': '#C6C6C6',
+        // 'light-gray': '#F5F5F5',
+        // 'primary': '#005B60',
+        // 'secondary': '#00ADAD',
+        // 'accent': '#ABABAB',
+        // 'background': '#000000',
+        // 'background': '#274472',
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
