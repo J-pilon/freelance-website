27b46d5ab2c61eb1affc93883dd4d471cb3d04d7
Josiah <josiah.pilon@gmail.com>
2026-04-01 17:38:10 -0700

    feat: adds services controller and views

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ServicesController < ApplicationController
+  def mvp_development
+  end
+
+  def startup_product_development
+  end
+
+  def prototype_to_production
+  end
+
+  def ai_automation
+  end
+end
